c8bc15795941185cb42749169cc569035c1a4d28
vini <viniciusicmsc@gmail.com>
2024-05-23 15:48:07 -0300

    Pokedex

--- src/components/Carta.jsx
+++ src/components/Carta.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react'
+import Status from './Status';
 
 export default function Carta() {
     const [id, setId] = useState(1);
@@ -22,27 +23,35 @@ export default function Carta() {
         <>
             {
                 pokemon && (
-                    <div className='Carta'>
-                        <nav>
-                            <span>#{pokemon.id}</span>
-                            <ul>
-                                <li>
-                                    {pokemon.types.map((type, index) => (
-                                        <p key={index}>{type.type.name}</p>
-                                    ))}
-                                </li>
-                            </ul>
-                        </nav>
-                        <div className='Status'>
-                            <h1>{pokemon.name}</h1>
-                            <img src={pokemon.sprites.front_default} />
-                            <div>
-                            <button onClick={() => setId(id - 1)}>Anterior</button>
-                            <button onClick={() => setId(id + 1)}>Próximo</button>
-                            </div>
+                    <>
+                        <div className='Carta'>
+                            <nav>
+                                <span>#{pokemon.id}</span>
+                                <ul>
+                                    <li>
+                                        {pokemon.types.map((type, index) => (
+                                            <p key={index}>{type.type.name}</p>
+                                        ))}
+                                    </li>
+                                </ul>
+                            </nav>
+                            <section>
+                                <h1>{pokemon.name}</h1>
+                                <img src={pokemon.sprites.front_default} alt={pokemon.name} />
+                                <Status
+                                    weight={pokemon.weight}
+                                    hp={pokemon.stats[0].base_stat}
+                                    attack={pokemon.stats[1].base_stat}
+                                    defense={pokemon.stats[2].base_stat}
+                                    speed={pokemon.stats[5].base_stat}
+                                />
+                            </section>
                         </div>
-
-                    </div>
+                        <div className='Buttons'>
+                            <button onClick={() => setId(id > 1 ? id - 1 : 1)}>Anterior</button>
+                            <button onClick={() => setId(id + 1)}>Próximo</button>
+                        </div>
+                    </>
                 )
             }
         </>
