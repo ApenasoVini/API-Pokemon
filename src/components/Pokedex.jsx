a6c4e49759aa6598047d036a7b44471c9fa83501
VINÍCIUS SOARES FRANÇA <vinicius_s_franca@estudante.sesisenai.org.br>
2024-05-23 12:07:42 -0300

    Pokedex

--- src/components/Pokedex.jsx
+++ src/components/Pokedex.jsx
@@ -1,7 +1,39 @@
+import React, { useEffect, useState } from 'react'
+
 export default function Pokedex() {
+    const [id, setId] = useState(1);
+    const [pokemon, setPokemon] = useState(null)
+
+    const fetchData = async () => {
+        try {
+            const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
+            const data = await response.json();
+            setPokemon(data);
+        } catch (error) {
+            console.error(`Erro: ${error}`);
+        }
+    }
+
+    useEffect(() => {
+        fetchData()
+    }, [id])
+
     return (
-        <div>
-            <h1>Pokémon</h1>
-        </div>
+        <>
+            <button onClick={() => setId(id - 1)}>Anterior</button>
+            <button onClick={() => setId(id + 1)}>Próximo</button>
+
+            {
+                pokemon && (
+                    <div className="Pokemon">
+                        <h1>Pokémon</h1>
+                        <p>{pokemon.name}</p>
+                        {pokemon.types.map((type, index) => (
+                            <p key={index}>{type.type.name}</p>
+                        ))}
+                    </div>
+                )
+            }
+        </>
     )
 }
