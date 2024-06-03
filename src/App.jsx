ba517116fbc031a67eeee61b9830eb33a6ce22c5
vini <viniciusicmsc@gmail.com>
2024-06-03 14:34:43 -0300

    Spinner

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,48 @@
-import React from 'react'
 import './App.css'
+import React, { useState, useEffect } from 'react'
+import style from 'styled-components'
+import { motion } from "framer-motion"
 import Carta from './components/Carta'
 
+const Spinner = style(motion.div)`
+  width: 3rem;
+  height: 3rem;
+  border: 4px solid black;
+  border-top: 4px solid red;
+  border-radius: 50%;
+  display: inline-block;
+`;
+
 function App() {
+  const [carregando, setCarregando] = useState(true);
+
+  useEffect(() => {
+    const tempo = setTimeout(() => {
+      setCarregando(false);
+    }, 4000)
+    return () => clearTimeout(tempo);
+  }, [])
 
   return (
     <>
-      <Carta />
+      {carregando ? (
+        <Spinner
+          animate={{ rotate: 360 }}
+          transition={{ duration: 0.5, repeat: Infinity, ease: 'linear' }}
+        ></Spinner>
+      ) : (
+        <Carta />
+      )}
+
     </>
   )
 }
 
+
 export default App
+
+
+
+
+
+
